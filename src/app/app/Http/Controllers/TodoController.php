ef7194d4a030d2edb5a304b337121e7b41a9c3d2
kentozaka46 <kentoball52@gmail.com>
2022-08-21 20:44:12 +0900

    CRUD処理のAPI製造

--- src/app/app/Http/Controllers/TodoController.php
+++ src/app/app/Http/Controllers/TodoController.php
@@ -9,8 +9,58 @@ class TodoController extends Controller
 {
     public function index(Request $request)
     {
-      $todo_list = Todo::all();
-
-      return view('todos.index', ['todos', $todo_list]);
+      $todos = Todo::all();
+      return response()->json(
+        $todos, 200
+    );
     }
+
+    public function store(Request $request)
+	{
+        $todo = Todo::create($request->all());
+        return response()->json(
+            $todo, 201
+        );
+	}
+
+	public function show($id)
+	{
+        $todo = Todo::find($id);
+        return response()->json(
+            $todo, 200
+        );
+	}
+
+	public function update(Request $request, $id)
+	{
+        $update = [
+            'title' => $request->title,
+            'content' => $request->content
+        ];
+        $todo = Todo::where('id', $id)->update($update);
+        $todos = Todo::all();
+        if ($todo) {
+            return response()->json(
+                $todos
+            , 200);
+        } else {
+            return response()->json([
+                'message' => 'TODOが見つかりませんでした。',
+            ], 404);
+        }
+	}
+
+	public function destroy($id)
+	{
+        $todo = Todo::where('id', $id)->delete();
+        if ($todo) {
+            return response()->json([
+                'message' => 'TODOを削除しました。',
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'TODOが見つかりませんでした。',
+            ], 404);
+        }
+	}
 }
